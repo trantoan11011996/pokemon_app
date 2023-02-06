bf861e9c3ed8b714ba18a521f7e693348abb7492
toan <trantoan11011996@gmail.com>
2023-02-06 22:41:09 +0700

    update base app

---
+++ pokemon_app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainApp from './MainApp/MainApp';
+
+function App() {
+  return (
+    <div className="App">
+     <MainApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ pokemon_app/src/MainApp/MainApp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+export default function MainApp(){
+    
+    return(
+        <div className="Main-app">
+            Pokemon App
+        </div>
+    )
+}
